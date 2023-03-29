b2e3688e38fe3f330af993e68d4b057fe7613b33
Ricardo Vasquez <ricardovasquez@Ricardos-MacBook-Air.local>
2023-03-29 13:37:55 -0500

    load balancer

--- ticket-service/src/main/java/com/revendedor/ticketservice/service/interf/APIClient.java
+++ ticket-service/src/main/java/com/revendedor/ticketservice/service/interf/APIClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "USER-SERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "USER-SERVICE")
+@FeignClient(name = "USER-SERVICE")
 public interface APIClient {
     @GetMapping("/findById/{userId}")
     UserDto findById(@PathVariable("userId") int userId);
