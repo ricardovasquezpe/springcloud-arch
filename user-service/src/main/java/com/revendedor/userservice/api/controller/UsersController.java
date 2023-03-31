9316649e28d324b7e062c02e4da81cd3964cd322
Ricardo Vasquez <ricardovasquez@Ricardos-MacBook-Air.local>
2023-03-31 14:48:00 -0500

    elk

--- user-service/src/main/java/com/revendedor/userservice/api/controller/UsersController.java
+++ user-service/src/main/java/com/revendedor/userservice/api/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.revendedor.userservice.api.controller;
 
 import com.revendedor.userservice.application.dto.UserDto;
 import com.revendedor.userservice.service.interf.UsersService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping
+@Slf4j
 public class UsersController {
 
     @Autowired
@@ -21,11 +24,16 @@ public class UsersController {
 
     @GetMapping("/findAll")
     public ResponseEntity<List<UserDto>> findAll(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("This Logs page findAll " + localDateTime);
         return new ResponseEntity<List<UserDto>>(usersService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/findById/{userId}")
     public ResponseEntity<UserDto> findById(@PathVariable("userId") int userId){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("This Logs page findById " + localDateTime);
+
         UserDto dto = usersService.findById(userId);
         return new ResponseEntity<UserDto>(dto, HttpStatus.OK);
     }
