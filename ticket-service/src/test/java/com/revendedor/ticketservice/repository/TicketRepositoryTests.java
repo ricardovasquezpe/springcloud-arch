6cbc0fd6a3fd2c1f535eed760fa7fd482220ae31
Ricardo Vasquez <ricardovasquez@Ricardos-MacBook-Air.local>
2023-04-03 16:09:42 -0500

    repository test

--- ticket-service/src/test/java/com/revendedor/ticketservice/repository/TicketRepositoryTests.java
+++ ticket-service/src/test/java/com/revendedor/ticketservice/repository/TicketRepositoryTests.java
@@ -2,13 +2,14 @@ package com.revendedor.ticketservice.repository;
 
 import com.revendedor.ticketservice.repository.entity.Ticket;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 public class TicketRepositoryTests {
@@ -16,21 +17,26 @@ public class TicketRepositoryTests {
     @Autowired
     TicketRepository ticketRepository;
 
-    @DisplayName("UnitTest for save a new ticket")
-    @Test
-    public void givenTicketData_whenSave_thenReturnSavedTicket(){
-        Ticket ticket = new Ticket();
+    Ticket ticket;
+
+    @BeforeEach
+    public void setup(){
+        ticket = new Ticket();
         ticket.setUserId(2);
         ticket.setCode("BBB");
         ticket.setCreatedAt(Instant.now());
+    }
 
+    @DisplayName("UnitTest for save a new ticket")
+    @Test
+    public void givenTicketData_whenSave_thenReturnSavedTicket(){
         Ticket newTicket = ticketRepository.save(ticket);
 
         Assertions.assertThat(newTicket).isNotNull();
         Assertions.assertThat(newTicket.getId()).isGreaterThan(0);
     }
 
-    @DisplayName("UnitTest for findAll method")
+    @DisplayName("UnitTest for findAll tickets")
     @Test
     public void givenTicketList_whenFindAll_thenReturnList(){
         Ticket ticket1 = new Ticket();
@@ -50,5 +56,38 @@ public class TicketRepositoryTests {
 
         Assertions.assertThat(ticketList).isNotNull();
         Assertions.assertThat(ticketList.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Unit Test for find a ticket by ID")
+    @Test
+    public void givenTicketData_whenFindById_thenReturnTicketData(){
+        Ticket newTicket = ticketRepository.save(ticket);
+
+        Ticket ticketDB = ticketRepository.findById(newTicket.getId()).get();
+
+        Assertions.assertThat(ticketDB).isNotNull();
+    }
+
+    @DisplayName("Unit Test for update ticket data")
+    @Test
+    public void givenTicketData_whenUpdateTicket_thenReturnUpdatedTicket(){
+        Ticket newTicket = ticketRepository.save(ticket);
+
+        newTicket.setCode("CCC");
+        Ticket newTicketUpdated = ticketRepository.save(newTicket);
+
+        Assertions.assertThat(newTicketUpdated).isNotNull();
+        Assertions.assertThat(newTicketUpdated.getCode()).isEqualTo(newTicket.getCode());
+    }
+
+    @DisplayName("Unit Test for delete a ticket by ID")
+    @Test
+    public void given_when_then(){
+        Ticket newTicket = ticketRepository.save(ticket);
+
+        ticketRepository.deleteById(newTicket.getId());
+        Optional<Ticket> findTicket = ticketRepository.findById(newTicket.getId());
+
+        Assertions.assertThat(findTicket).isEmpty();
     }
 }
