71130e5ea28addde93eaa5d62d19b5e4dc253d3d
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2023-04-06 15:44:26 -0500

    profiles

--- ticket-service/src/main/resources/db/migration/V2__init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tickets_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(45) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
