c7f41dd1d348adf7a281171e6761f01797c8cb19
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2023-04-10 18:40:31 -0500

    ticket service docker

---
+++ init/01.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `rvdor_tickets`;
+GRANT ALL ON `rvdor_tickets`.* TO 'root'@'%';
--- ticket-service/src/main/resources/db/migration/V1__init.sql
+++ ticket-service/src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `tickets` (
+CREATE TABLE IF NOT EXISTS `tickets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(45) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
