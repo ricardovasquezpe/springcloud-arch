710fd8cff1efde21479dc0fb561feecf77df2b61
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2023-04-10 20:41:33 -0500

    docker compose kafka

--- init/01.sql
+++ init/01.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `rvdor_tickets`;
 GRANT ALL ON `rvdor_tickets`.* TO 'root'@'%';
+CREATE DATABASE IF NOT EXISTS `rvdor_payments`;
+GRANT ALL ON `rvdor_payments`.* TO 'root'@'%';
